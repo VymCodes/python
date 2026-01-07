80a68bfba2d3e9ba30c0a113be37721f27866eb7
VymCodes <vyomgupta1702@gmail.com>
2026-01-07 16:13:13 -0500

    i made more variables

--- Variables.py
+++ Variables.py
@@ -2,3 +2,20 @@ a=5
 b=10
 print(a+b)
 print(a-b)
+
+
+x, y = 50, 10
+print("x+y=", x+y)
+print(f"x+y = {x+y}")
+
+print("x-y=", x-y)
+print(f"x-y = {x-y}")
+
+print("x*y=", x*y)
+print(f"x*y = {x*y}")
+
+print("x/y=", x/y)
+print(f"x/y = {x/y}")
+
+print("y/x=", y/x)
+print(f"y/x = {y/x}") 
