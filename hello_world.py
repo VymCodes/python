797368053dfe5e5911bc5dd3163730a35795daf3
VymCodes <vyomgupta1702@gmail.com>
2026-01-07 15:54:47 -0500

    I made 2 variables names a and b

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,12 @@
 # below the code, I wrote a print statement.
 print("hello world")
+
+print("Hi!, my name is Vyom")
+
+print('Hi! my name is Vyom')
+
+print("I'm Vyom")
+
+print("Hi!, I'm Vyom. \nI like python")      
+
+print("Here I begin a tab space, \tNow do you see. the gap?")
