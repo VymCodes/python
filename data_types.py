131f1e81c31dce363affc7c8a3a251344162a23d
VymCodes <vyomgupta1702@gmail.com>
2026-01-11 11:10:24 -0500

    I made a string

---
+++ data_types.py
@@ -0,0 +1,19 @@
+# data_types.py
+my_number = 78
+print(my_number)
+print(type(my_number))
+
+print(f"The type of {my_number} is {type(my_number)}")
+
+passed = True   # False is also boolean
+print(passed) # this line print variable passed
+print(type(passed))
+
+name = "vyom"   #observe the double quote
+print(name)
+print(type(name))
+
+temperature = 98.7  # it has decimal values
+print(temperature)
+print(type(temperature))
+
---
+++ strings.py
@@ -0,0 +1,6 @@
+statement1 = "I like python"
+statement2 = " But i dont like java"
+
+print(statement1 + statement2)
+print(statement1, end=" ")
+print(statement2)
