6ea1c6b8ac212cc42d27192f110201bc85d86d0c
VymCodes <vyomgupta1702@gmail.com>
2026-01-14 16:09:53 -0500

    I used some Operators.

---
+++ operaters.py
@@ -0,0 +1,46 @@
+x = 2
+y = 4
+
+print(x**y) # 2**2 = 4, 2**3 = 8, 2**4 = 16
+
+a = 7
+b = 3
+
+print(a/b)
+print(a//b) # floor value , means nearest low
+
+# % gives remainder
+
+print(5%2)
+
+vyom_score = 95
+print(vyom_score)
+print("Vyom studied and scored", vyom_score)
+print(f"Vyom studied and scored : {vyom_score}")
+print("Vyom studied and scored: {}".format(vyom_score))
+print(5 == 5)
+print(5 == 98)
+print(5 != 9)
+print(5 != 5)
+
+y = 6
+x = 5
+
+print(x == y)
+print(x != y)
+print((x!=y) == (y!=x))
+print((x == y) == (y == x))
+
+x = 10
+y = 5
+
+print(x > y) # True
+print(y >x) # False
+print((x > y) == (y < x)) # True
+print((x < y) !=  (x > y)) # True
+
+x = 15
+y = 15
+
+print(x>=y)
+print(y<=x)
