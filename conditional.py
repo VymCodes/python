f0d8e012cf0da8ac6276b3f57124c76ce028000d
VymCodes <vyomgupta1702@gmail.com>
2026-01-19 11:03:10 -0500

    This is my genourous grading system

---
+++ conditional.py
@@ -0,0 +1,21 @@
+# age = int(input("enter your age : "))
+# print(f"your age is {age}")
+# print(type(age))
+# if age >= 18:
+#     print("You can vote")
+# else:
+#     print("You are too young to vote")
+
+score = int(input("Enter your score : "))
+print(f"your score is {score}%")
+if score >= 80:
+    print("You got an A!")
+elif score >= 60:
+    print("You got an B!")
+elif score >= 40:
+    print("You got a C!")
+elif score >= 20:
+    print("You got a D")
+else:
+    print("F,You have failed horribly")
+
