7058083f7999ad976a4c62585a9a71853b7488c1
VymCodes <vyomgupta1702@gmail.com>
2026-01-25 11:10:05 -0500

    How to find your job

--- conditional2.py
+++ conditional2.py
@@ -8,12 +8,25 @@
 # print(c in b)
 # print(c not in a)
 
-x = 4.1
-if type(x) is int:
-    print("X is a number")
-elif type(x) is float:
-    print("X is a decimal")
-elif type(x) is bool:
-    print("X is a boolean value")
-elif type(x) is str:
-    print("X is a string")
+# x = 4.1
+# if type(x) is int:
+#     print("X is a number")
+# elif type(x) is float:
+#     print("X is a decimal")
+# elif type(x) is bool:
+#     print("X is a boolean value")
+# elif type(x) is str:
+#     print("X is a string")
+
+math = int(input("Enter score for maths : "))
+science = int(input("Enter score for science : "))
+if (90 < math < 100) and science < 60:
+    print("You can take engineering, but not science")
+elif math < 90 and science > 90:
+    print("You cant be a mathematician, but you can be a scientist")
+elif (0 < math < 50) and (0 < science < 50):
+    print("You cant get a job")
+elif math == 100 or science == 100:
+    print("You are very smart, you can pick anything")
+elif math == 0 or science == 0:
+    print("Something is wrong")
