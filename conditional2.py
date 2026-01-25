56666b25ee316b7c0e3c60226785a1f3fe38d5e7
VymCodes <vyomgupta1702@gmail.com>
2026-01-25 10:52:06 -0500

    conditional 2

---
+++ conditional2.py
@@ -0,0 +1,19 @@
+# a = [1,2,3,4,5]
+# b = [1,2,3,4,5,6,7,8,9,10]
+# c = 7
+
+# print(a in b)
+# print(a == b)
+# print(c in a)
+# print(c in b)
+# print(c not in a)
+
+x = 4.1
+if type(x) is int:
+    print("X is a number")
+elif type(x) is float:
+    print("X is a decimal")
+elif type(x) is bool:
+    print("X is a boolean value")
+elif type(x) is str:
+    print("X is a string")
