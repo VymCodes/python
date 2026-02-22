c107ae25a5a87061127acdd5c4cde82dd4cfa119
VymCodes <vyomgupta1702@gmail.com>
2026-02-22 10:22:37 -0500

    square

---
+++ turtle library/square_hw.py
@@ -0,0 +1,16 @@
+import turtle
+
+
+turtle.Screen().bgcolor("blue")
+turtle.Screen().setup(1200,200)
+square = turtle.Turtle()
+
+num_sides = 4
+side_length = 88
+angle = 360.0 / num_sides
+
+for i in range(num_sides):
+    square.forward(side_length)
+    square.right(angle)
+
+turtle.exitonclick() #hi its vyom and i likle turtles
