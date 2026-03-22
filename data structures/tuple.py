1c985f08db126df4e39bce05acf874573c0403fb
VymCodes <vyomgupta1702@gmail.com>
2026-03-22 11:09:56 -0400

    Tuple

---
+++ data structures/tuple.py
@@ -0,0 +1,16 @@
+number_tuple = (10, 20, 25.75)
+print(len(number_tuple))
+
+
+
+string_tuple = ("jessa", "Emma", "kelly")
+print(len(string_tuple))
+
+tuple1 = ("p", "y", "t", "h", "o", "n", )
+for i in range(4):
+    print(tuple1[i])
+
+
+tuple1 = ("p", "y", "t", "h", "o", "n", )
+
+print(tuple1[2:5])
---
+++ data structures/tuple2.py
@@ -0,0 +1,12 @@
+tuple1 = ("p", "y", "t", "h", "o", "n", )
+tuple2 = (1, 2, 3)
+
+print(tuple1+tuple2)
+
+
+tuple1 = ("p", "y", "t", "h", "o", "n", )
+
+tuple1[2] = 'g'
+print(tuple1[2])
+
+
