e531ddbc5f6e45c33c8b764dc95cf6df7e94c50d
VymCodes <vyomgupta1702@gmail.com>
2026-03-25 16:08:24 -0400

    Dictionary

---
+++ data structures/dictionary.py
@@ -0,0 +1,12 @@
+my_dict = {"name" : "Jack", "age" : 26}
+
+print(my_dict["name"])
+print(my_dict["age"])
+
+my_dict = {"Name": "Zara", "age" : 7, "Class": "First"}
+
+my_dict["age"] = 6
+
+my_dict["gender"] = "female"
+
+print("Dictionary-", my_dict)
